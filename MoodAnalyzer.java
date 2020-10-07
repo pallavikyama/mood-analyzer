20557cddc8c7891c99b40896c08f66bbe0e6e75a
pallavikyama <kyamap98@gmail.com>
2020-10-07 16:06:45 +0530

    Added:analyzeMood function is added to analyze if the given message is happy or sad

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,7 +1,28 @@
 package com.blz.moodanalyzer;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MoodAnalyzer {
+
+	static Scanner sc = new Scanner(System.in);
+
+	// UC1
+	public static String analyseMood(String text) {
+		Pattern code = Pattern.compile(".*[Ss][Aa][Dd].*");
+		Matcher check = code.matcher(text);
+		if (check.matches())
+			return "SAD";
+		else
+			return "HAPPY";
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to MOOD ANALYZER.");
+		System.out.println("Enter mood related message:");
+		String message = sc.nextLine();
+		System.out.println(MoodAnalyzer.analyseMood(message));
+		sc.close();
 	}
 }
